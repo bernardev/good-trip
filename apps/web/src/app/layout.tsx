accb6f0e923520104f88e29addcd4c20bb1cbd4b
bernardev <ebernardes.dev@gmail.com>
2025-09-28 21:21:29 -0300

    tag ads

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -15,11 +15,37 @@ export const metadata: Metadata = {
   description: "Passagens, hospedagens, pacotes, carros e eventos.",
 };
 
+const GTM_ID = "GTM-TGXL5FVV";
+
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="pt-BR" className="h-full overflow-x-hidden">
-      <head>{/* Distribusion SDK assets */}</head>
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="gtm" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
+        {/* Distribusion SDK assets */}
+      </head>
       <body className={`${dmSans.className} min-h-dvh antialiased bg-bg text-ink overflow-x-hidden`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <Providers>
           <IntroReveal />
           <Header />
